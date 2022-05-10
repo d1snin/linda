6db1b69652de44df70e2e3a53f82b3465bc93d87
d1snin <me@d1s.dev>
2022-05-10 03:38:06 +0300

    switched to java 16 jvm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "16"
     }
 }
 
