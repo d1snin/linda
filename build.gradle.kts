27ff249b5d0666a3640436c33584f3b2d1976e48
d1snin <me@d1s.dev>
2022-05-24 17:17:54 +0300

    bumped spring plugin to `2.7.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    id("org.springframework.boot") version "2.6.7"
+    id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.6.21"
-    id("org.springdoc.openapi-gradle-plugin") version "1.3.4"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
 }
